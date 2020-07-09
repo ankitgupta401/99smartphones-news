c5f0a3e4a099732c9e465a97f401dbfb0fbd2c54
SANJAY SARKAR <sanjaysarkar430@gmail.com>
2020-07-09 13:28:12 +0530

    Footer and An API complete

---
+++ components/Header.js
@@ -0,0 +1,12 @@
+const Header = () => (
+  <div className="container-fluid">
+    <div className="navbar-div">
+      <ul className="navbar-ul">
+        <li>Home</li>
+        <li>About</li>
+      </ul>
+    </div>
+  </div>
+);
+
+export default Header;
---
+++ components/Layout.js
@@ -0,0 +1,25 @@
+import Head from "next/head";
+import Header from "./Header";
+import Footer from "./Footer";
+
+const Layout = (props) => (
+  <>
+    <Head>
+      <title>99Smartphones News</title>
+      <link
+        rel="stylesheet"
+        href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+      />
+      <link
+        ref="stylesheet"
+        href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+      />
+    </Head>
+    <Header />
+    <div className="container-fluid"></div>
+    {props.children}
+    <Footer />
+  </>
+);
+
+export default Layout;
