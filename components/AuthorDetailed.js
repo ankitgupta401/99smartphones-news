7e2b027dc900d4bc9868dc4408c31c53cbc64659
Ankit Gupta <ag3831124@gmail.com>
2020-07-31 23:20:48 +0530

    data rendering

--- components/AuthorDetailed.js
+++ components/AuthorDetailed.js
@@ -1,25 +1,23 @@
 import React from "react";
 
-const AuthorDetailed = () => {
+const AuthorDetailed = (props) => {
   return (
     <div style={{ backgroundColor: "rgba(0,0,0,.1)",padding:"30px 50px",display:"flex" }}>
       <div style={{margin:"10px"}}>
         <img
-          style={{ borderRadius: "50%",width:"100%" }}
-          src="https://picsum.photos/400"
+          style={{ borderRadius: "50%",width:"100%" ,maxWidth:"100px"}}
+          src={props.image2}
           alt=""
         />
+        {/* {console.log(props)} */}
       </div>
       <div style={{margin:"10px"}}>
-        <h3 style={{fontSize:"27px",fontWeight:"bolder"}}>Joanna Taylor</h3>
+  <h3 style={{fontSize:"27px",fontWeight:"bolder"}}>{props.name}</h3>
         <p style={{fontSize:"17px",fontWeight:"normal"}}>
-          Distinctively exploit optimal alignments for intuitive bandwidth.
-          Quickly coordinate e-business applications through revolutionary
-          catalysts for change. Seamlessly underwhelm optimal testing procedures
-          whereas bricks-and-clicks processes
+         {props.designation}
         </p>
         <div style={{display:"flex"}}>
-          <a>VIEW ALL POSTS</a>
+          <a href={ "../author/" + props.email }>VIEW ALL POSTS</a>
           <i className=""></i>
         </div>
       </div>
--- pages/[...slug].js
+++ pages/[...slug].js
@@ -1,9 +1,9 @@
 import { useRouter } from "next/router";
 import Layout from "../components/Layout";
 import AuthorDetailed from "../components/AuthorDetailed";
-//
+import * as url from "../pages/api.json";
 // posts will be populated at build time by getStaticProps()
-const Pages = () => {
+const Pages = (props) => {
   const router = useRouter();
   const { slug } = router.query;
 
@@ -14,7 +14,8 @@ const Pages = () => {
           <div className="container">
             <h1 style={{ lineHeight: "150px", margin: "0px 40px" }}>
               {/* {slug[1]} */}
-              Header Of the Title
+           {/* {console.log(props)} */}
+           {props.news.title}
             </h1>
           </div>
         </div>
@@ -24,9 +25,31 @@ const Pages = () => {
             <div className="col-sm-8 col-md-8 col-lg-8"></div>
             <div className="col-sm-2 col-md-2 col-lg-2"></div>
           </div>
+          {/* All The Content Begins Here */}
+<div>
+<img src={props.news.mainImage} style={{height:"400px"}} alt={props.news.alt_image ?props.news.alt_image:''  } />
+{/* Printing The paragraphs */}
+{props.paras.map((val, i) => {
+     return (
+       <div key={i}> 
+  <br/>
+        {/* Subtitle for the news paragraph */}
+       <h2 style={{color:"black"}}> {props.paras[i].sub_title}</h2>
+       <br/>
+       {/* image for the paragraph */}
+        {props.paras[i].image ?   <img src={props.paras[i].image} style={{height:"400px"}} alt={props.paras[i].sub_title} />:''}
+          {/* content of the paragraph */}
+          <div dangerouslySetInnerHTML={{ __html: props.paras[i].paras }} />
+        
+       </div>
+     )
+}) }
+
+</div>
+
           <div className="row">
             <div className="col-sm-10 col-md-10 col-lg-10">
-              <AuthorDetailed />
+              <AuthorDetailed {...props.writer}  />
             </div>
           </div>
         </div>
@@ -35,9 +58,61 @@ const Pages = () => {
   );
 };
 
-Pages.getInitialProps = ({ query }) => {
-  console.log(query.slug);
-  return { data: query };
+Pages.getInitialProps = async ({ query }) => {
+  const news = await fetch(url.url + "get_news_list", {
+    method: "POST",
+    // Adding body or contents to send
+    body: JSON.stringify({
+      no_of_news: 1,
+      page_no: 0,
+      search: "",
+      data: {
+        link: query.slug[1],
+      },
+    }),
+    // Adding headers to the request
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  });
+
+  let data = await news.json();
+let parasData = [];
+// console.log(data,  query.slug[1]);
+  for(let i=0;i< data.result[0].paras.length;i++){
+    const paras = await fetch(url.url + "get_paras", {
+      method: "POST",
+      // Adding body or contents to send
+      body: JSON.stringify({
+       "filename": data.result[0].paras[i].content
+      }),
+      // Adding headers to the request
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    });
+   
+  let dataParas = await paras.json();
+ ;
+  parasData.push({sub_title:data.result[0].paras[i].sub_title,  image : data.result[0].paras[i].image, paras: dataParas.result});
+  }
+ 
+  const writerapi = await fetch(url.url + "common_get_with_table_name", {
+    method: "POST",
+    // Adding body or contents to send
+    body: JSON.stringify({
+      table: "writer",
+      data: {},
+    }),
+    // Adding headers to the request
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  });
+
+  const writer = await writerapi.json();
+
+  return { data: query , news: data.result[0], paras: parasData, writer: writer.result[0]};
   //...
 };
 
