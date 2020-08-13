99ff91856bdd9481bfc17661b57b08589a9e3e4f
SANJAY SARKAR <sanjaysarkar430@gmail.com>
2020-08-13 12:28:06 +0530

    Mobile Nav Complete

--- components/Navbar.js
+++ components/Navbar.js
@@ -13,6 +13,11 @@ const handleBackdrop = () => {
   element.classList.remove("Open");
 };
 
+const mobileOpen = () => {
+  let element = document.getElementById("mobile-nav");
+  element.classList.toggle("mobile-nav-open");
+};
+
 const Navbar = (props) => {
   useEffect(() => {
     startTime();
@@ -72,18 +77,61 @@ const Navbar = (props) => {
       }, 1000);
     }
   }
+
   return (
     <>
       <div id="nav">
-        <a id="dateNav"></a>
-        <br />
-        <a id="timeNav"></a>
-        <p>Hello Hi</p>
+        <h1 style={{ color: "#000", padding: "20px 20px 0px 20px" }}>
+          99 News
+        </h1>
+        <Link href="/">
+          <a className="mobile-nav-link">Home</a>
+        </Link>
+        <a className="mobile-nav-link" onClick={mobileOpen}>
+          Categories
+        </a>
+        <div id="mobile-nav">
+          {props.data.category.result.map((val, i) => {
+            return (
+              <Link href={"/" + val.category}>
+                <a className="navbar-link mobile">{val.category}</a>
+              </Link>
+            );
+          })}
+        </div>
+        <a
+          className="mobile-nav-link"
+          href="https://99smartphones.in"
+          target="_blank"
+        >
+          Compare
+        </a>
+        <a
+          className="mobile-nav-link"
+          href="https://99smartphones.in"
+          target="_blank"
+        >
+          Product Finder
+        </a>
+        <a
+          className="mobile-nav-link"
+          href="https://99smartphones.in"
+          target="_blank"
+        >
+          Deals
+        </a>
+        <a
+          className="mobile-nav-link"
+          href="https://blog.99smartphones.in"
+          target="_blank"
+        >
+          Blogs
+        </a>
       </div>
       <div className="burger" id="burgerId" onClick={handleSidebar}>
         <i aria-hidden className="burger-icon fa fa-bars"></i>
       </div>
-    
+
       <div onClick={handleBackdrop} className="container-fluid-navq">
         <div className="container">
           <nav className="navbar">
@@ -95,7 +143,6 @@ const Navbar = (props) => {
                   </Link>
                 </li>
                 <li>
-             
                   <Link href="/">
                     <a>News Categories</a>
                   </Link>
@@ -117,13 +164,19 @@ const Navbar = (props) => {
                   </a>
                 </li>
                 <li>
-                  <a href="https://99smartphones.in" target="_blank">Product Finder</a>
+                  <a href="https://99smartphones.in" target="_blank">
+                    Product Finder
+                  </a>
                 </li>
                 <li>
-                  <a href="https://99smartphones.in" target="_blank">Deals</a>
+                  <a href="https://99smartphones.in" target="_blank">
+                    Deals
+                  </a>
                 </li>
                 <li>
-                  <a href="https://blog.99smartphones.in" target="_blank">Blogs</a>
+                  <a href="https://blog.99smartphones.in" target="_blank">
+                    Blogs
+                  </a>
                 </li>
               </ul>
             </div>
