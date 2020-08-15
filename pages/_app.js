b3b88d45929a0a8ae26d2f2344dd3cc10c6cb921
Ankit Gupta <ag3831124@gmail.com>
2020-08-15 14:24:34 +0530

    some change

--- pages/_app.js
+++ pages/_app.js
@@ -35,14 +35,14 @@ export default function MyApp({ Component, pageProps }) {
 
   React.useEffect(() => {
  
-    // if ('serviceWorker' in navigator) {
-    //   navigator.serviceWorker.register('../firebase-messaging-sw.js')
-    //     .then(function(registration) {
-    //       console.log('Registration successful, scope is:', registration.scope);
-    //     }).catch(function(err) {
-    //       console.log('Service worker registration failed, error:', err);
-    //     });
-    //   }
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('../firebase-messaging-sw.js')
+        .then(function(registration) {
+          console.log('Registration successful, scope is:', registration.scope);
+        }).catch(function(err) {
+          console.log('Service worker registration failed, error:', err);
+        });
+      }
   
     const messaging = firebase.messaging()
     messaging.requestPermission().then(() => {
