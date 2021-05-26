3ebaf48879cadb0ebcb26aed6d4fb0a4a53536ce
Ankit Gupta <ag3831124@gmail.com>
2021-05-27 02:55:12 +0530

    some chages

--- getUrl.js
+++ getUrl.js
@@ -3,7 +3,7 @@ const url2= "https://smartphones99-node-2.herokuapp.com/api/common/" ;
 
 
 export const getURL =() => {
-    const newUrl = (new Date().getDate())%2 ==0 ? url2 : url
+    const newUrl = (new Date().getDate())-1 %2 ==0 ? url2 : url
     return newUrl;
     // return "http://localhost:5000/api/common/"
 }
