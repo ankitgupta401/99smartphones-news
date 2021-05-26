fdef10d2c2e084e5e69d070939aa091c6efeae33
Ankit Gupta <ag3831124@gmail.com>
2021-05-26 22:27:22 +0530

    convert to dynamic blog

--- getUrl.js
+++ getUrl.js
@@ -4,7 +4,7 @@ import * as urls from "./api.json";
 
 
 export const getURL =() => {
-    // return (new Date().getDate())%2 ==0? urls.url : urls.url2;
-    return "http://localhost:5000/api/common/"
+    return (new Date().getDate())%2 ==0? urls.url : urls.url2;
+    // return "http://localhost:5000/api/common/"
 }
 
