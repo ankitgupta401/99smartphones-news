6a7a409c904139fa0a4d5422ed4a59d022cdac9d
Ankit Gupta <ag3831124@gmail.com>
2021-06-28 15:08:01 +0530

    some changes

--- utils/getUrl.js
+++ utils/getUrl.js
@@ -2,9 +2,14 @@ const url ="https://smartphones-99-node.herokuapp.com/api/common/";
 const url2= "https://smartphones99-node-2.herokuapp.com/api/common/" ;
 
 
+
 export const getURL =() => {
-    const newUrl = (new Date().getUTCDate()) <= 22 ? url2 : url
+    let date = (new Date().getUTCDate());
+
+    const newUrl =  date <= 25 ? url2 : url
    return newUrl;
     // return "http://localhost:5000/api/common/"
 }
+
+
 
